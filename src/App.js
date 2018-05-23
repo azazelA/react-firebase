b20d815752a0c7453b7787a14e32575b3738ed20
ahmad <ahmadhabib@punch.cool>
2018-05-23 13:47:44 +0500

    importing home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import fire from './config/Fire';
 import Login from './components/Login';
-import Home from './components/Home'
+import Home from './components/Home';
 import './App.css';
 
 class App extends Component {
@@ -16,6 +16,7 @@ class App extends Component {
     }
     authListener(){
         fire.auth().onAuthStateChanged((user) => {
+            console.log(user)
             if(user){
                 this.setState({user})
             } else {
@@ -28,7 +29,7 @@ class App extends Component {
       document.title = 'Firebase Demo'
     return (
       <div className="App">
-          {this.state.user ? <Home /> : <Login />}
+          {this.state.user ? (<Home />) : (<Login />)}
       </div>
     );
   }
