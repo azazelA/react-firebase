bf80a1655499f27d47e4f2c1c2d6ff81e0b10796
ahmad <ahmadhabib@punch.cool>
2018-05-23 13:46:34 +0500

    fixed submit onClick

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
+import fire from '../config/Fire';
 class Login extends Component {
     constructor (props){
         super(props)
         this.state = {
-            email: {},
-            password: {}
+            email: '',
+            password: ''
         }
+        this.login = this.login.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
 
     handleChange(e){
-        //e.preventDefault();
-        //this.setState()
+        this.setState({ [e.target.name]: e.target.value });
     }
 
+    login(e){
+        e.preventDefault();
+        fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+            .catch((error) => {
+                console.log(error)
+            })
+    }
     render() {
         document.title = 'Login Page'
         return (
@@ -20,14 +29,14 @@ class Login extends Component {
                 <form>
                     <div className="input">
                         <label htmlFor="InputEmail">Email</label>
-                        <input type="email" name="email" id="InputEmail" onChange={this.handleChange} value={this.state.email}/>
+                        <input type="email" name="email" id="InputEmail" onChange={this.handleChange} value={this.state.email} placeholder="Enter your email address"/>
                     </div>
                     <div className="input">
                         <label htmlFor="InputPassword">Password</label>
-                        <input type="password" name="password" id="InputPassword" onChange={this.handleChange} value={this.state.password}/>
+                        <input type="password" name="password" id="InputPassword" onChange={this.handleChange} value={this.state.password} placeholder="Set a password for your account"/>
                     </div>
                     <div className="input">
-                        <input type="submit" className="submit" onChange={this.login} />
+                        <input type="submit" className="submit" onClick={this.login} />
                     </div>
                 </form>
             </div>
