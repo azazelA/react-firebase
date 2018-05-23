e5bd685c3c560ba822d842286cd240204cb03ea1
ahmad <ahmadhabib@punch.cool>
2018-05-23 13:47:04 +0500

    added logout button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import fire from '../config/Fire';
+
 class Home extends Component {
+    constructor(props){
+        super(props);
+        this.logout = this.logout.bind(this)
+    }
+    logout(){
+        fire.auth().signOut();
+    }
     render() {
         document.title = 'Login Page'
         return (
-            <h1>You're finally home</h1>
+            <div className="Home">
+                <h1>You're finally home</h1>
+                <button onClick={this.logout}>Logout</button>
+            </div>
+
         );
     }
 }
